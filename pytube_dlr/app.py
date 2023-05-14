a5237b0f36b029a8f0c3a205faffdb2dd16fda8f
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-05-14 09:40:56 -0300

    docs: full documentation for main() function

--- pytube_dlr/app.py
+++ pytube_dlr/app.py
@@ -11,20 +11,39 @@ traceback_install()
 TInterfaceOptions = Literal['linux_cli', 'win_cli']
 
 
-# todo: add documentation
+# todo: ...
 def main(factory: IApplicationFactory) -> None:
+    """Main function that will trigger the events in order to do the entire
+    process of download some YouTube video.
+
+    :param IApplicationFactory factory: Factory that has the right methods to
+        handle the YouTube API, user arguments and handle with the file system.
+
+    Need to be done:
+    ----------------
+    - Make it verify if the user has past cache files to continue wight the
+      downloading process;
+        - If not, get the user arguments (it doesn't matter where it came from)
+          to work with;
+    - Using the argumetns, start the download process;
+
+    Developer notes:
+    ----------------
+    Every event inside this function need to be triggered by the factory
+    parammeter.
+    """
     args: IApplicationArgs = factory.get_arguments()
 
     print('|> I need to work with this object:')
     print(args)
 
 
-# todo: write tests
+# todo: create tests
 def setup(user_interface: TInterfaceOptions = 'linux_cli') -> None:
     """Function responsible for setting up the right factories to generate the
     right tools to the ``main()`` function use it.
 
-    :param Literal['linux_cli', 'win_cli'] interface: type of user interface to
+    :param Literal['linux_cli', 'win_cli'] interface: Type of user interface to
         factory.
     """
     FACTORIES: Dict[str, IApplicationFactory] = {
