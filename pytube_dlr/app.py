9c95c47ad733136f1c6b1b1b489d5ad6c1c2f530
Kevin Marques <kevinmarqueskmp@gmail.com>
2023-05-13 22:55:59 -0300

    impl: using the linux factory in setup function

--- pytube_dlr/app.py
+++ pytube_dlr/app.py
@@ -1,7 +1,36 @@
 #!/usr/bin/env python3
 
-# todo: add a markdown documentation to that project
-def start() -> None:
-    """
-    """
-    print('Hello world!')
+from pytube_dlr.models.abstracts import IApplicationFactory, IApplicationArgs
+from pytube_dlr.controllers.factories import LinuxCliApplicationFactory
+
+from typing import Literal, Dict
+from rich.traceback import install as traceback_install
+
+traceback_install()
+
+TInterfaceOptions = Literal['cli', 'linux_ui', 'win_ui']
+
+
+# todo: add documentation
+def main(factory: IApplicationFactory) -> None:
+    args: IApplicationArgs = factory.get_arguments()
+
+    print('|> I need to work with this object:')
+    print(args)
+
+
+# todo: add documentation
+# todo: write tests
+def setup(interface: TInterfaceOptions = 'cli') -> None:
+    FACTORIES: Dict[str, IApplicationFactory] = {
+        'cli': LinuxCliApplicationFactory(),
+        'linux_ui': None,
+        'win_ui': None
+    }
+
+    if interface in FACTORIES:
+        if FACTORIES[interface] is None:
+            raise "That interface doesn't exist yet"
+
+        factory: IApplicationFactory = FACTORIES[interface]
+        main(factory)
